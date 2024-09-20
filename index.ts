e852b84e1884bb269f6f78cee24881c15f528cc1
Timofei Benko <tsimafei.benko@ventionteams.com>
2024-09-20 18:12:01 +0200

    implement change status and help commands

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { exit } from 'process';
 import * as fs from 'node:fs';
 
-const COMMANDS = ['add', 'update', 'list', 'delete', 'mark-in-progress', 'mark-done']
-
-enum TASK_STATUSES {
+enum TaskStatus {
 		TODO = 'todo',
 		IN_PROGRESS = 'in-progress',
 		DONE = 'done',
@@ -12,7 +10,7 @@ enum TASK_STATUSES {
 type Task = {
 		id: number
 		description: string
-		status: TASK_STATUSES,
+		status: TaskStatus,
 		createdAt: string
 		updatedAt: string | null
 }
@@ -38,6 +36,32 @@ function check_if_task_exists(id: Task['id']) {
 function log_cli_error(commandName: string, error: string) {
 		console.error(`Error: ${commandName}: ${error}`)
 }
+
+function log_bad_id_error(commandName: string) {
+		log_cli_error(commandName, 'Task id must be a number')
+}
+
+function log_missing_id_error(commandName: string) {
+		log_cli_error(commandName, 'Task id was not provided')
+}
+
+function run_id_expection(commandName: string, parsedId: string) {
+		if (!parsedId) {
+				log_missing_id_error(commandName)
+				exit(1)
+		}
+
+		if (Number.isNaN(+parsedId)) {
+				log_bad_id_error(commandName)
+				exit(1)
+		}
+
+		if (!check_if_task_exists(+parsedId)) {
+				log_cli_error(commandName, `Task does not exist`)
+				exit(1)
+		}
+}
+
 
 function generate_task_id() {
 		if (!fileObj.tasks) {
@@ -71,13 +95,13 @@ function list_tasks(status?: Task['status']) {
 
 		tasksToPrint.forEach(({ id, status, description, createdAt, updatedAt }) => {
 				console.log(`${id}. ${description} (${status})`)
-				console.log(`(created: ${createdAt}, last updated: ${updatedAt}\n`)
+				console.log(`(created: ${createdAt}, last updated: ${updatedAt || '—'}\n`)
 		})
 }
 
 function add_task(description: Task['description']) {
 		const createdAt = get_date_now_locale_date_string()
-		const status = TASK_STATUSES.TODO
+		const status = TaskStatus.TODO
 		const existingTasks = fileObj.tasks || []
 
 		const tasks: Task[] = [
@@ -131,6 +155,45 @@ function delete_task(id: number) {
 		write_to_json(updatedTasks)
 }
 
+function change_status(id: number, status: TaskStatus) {
+		if (!fileObj.tasks) {
+				console.info('You dont have any tasks')
+				return
+		}
+
+		const updatedTasks = fileObj.tasks.map((task) => task.id === id ? { ...task, status } : task)
+		write_to_json(updatedTasks)
+}
+
+function log_help() {
+		console.info('Usage:\n')
+
+		console.info('add <task-description>')
+		console.info(`Adds a task\n`)
+
+		console.info('update <task-id> <task-description>')
+		console.info(`Updates task description\n`)
+
+		console.info('list <?status>')
+		console.info('Lists all tasks. If status parameter is provided, list only the tasks in that status.')
+		console.info(`Possible status values are ${Object.values(TaskStatus).join(', ')}\n`)
+
+		console.info('delete <task-id>')
+		console.info(`Deletes a task\n`)
+
+		console.info('mark-todo <task-id>')
+		console.info(`Changes task status to todo\n`)
+
+		console.info('mark-in-progress <task-id>')
+		console.info(`Changes task status to in-progress\n`)
+
+		console.info('mark-done <task-id>')
+		console.info(`Changes task status to done\n`)
+
+		console.info(`help`)
+		console.info(`Lists the list of available commands`)
+}
+
 for (let i = 2; i < process.argv.length; i++) {
 		const arg = process.argv[i]
 		const param1 = process.argv[i + 1]
@@ -144,64 +207,69 @@ for (let i = 2; i < process.argv.length; i++) {
 						}
 
 						add_task(param1)
-						break
+						exit(1)
 
 				case 'update':
-						if (!param1) {
-								log_cli_error('UPDATE', 'ask id was not provided')
-								exit(1)
-						}
-
-						if (Number.isNaN(+param1)) {
-								log_cli_error('UPDATE', 'Task id must be a number')
-						}
-
-						if (!check_if_task_exists(+param1)) {
-								log_cli_error('UPDATE', `Task with id ${param1} does not exist`)
-								exit(1)
-						}
+						run_id_expection('UPDATE', param1)
 
 						if (!param2) {
-								log_cli_error('UPDATE', 'Description was not provided')
+								log_cli_error('UPDATE', 'Task description was not provided')
 								exit(1)
 						}
 
 						update_task(+param1, param2)
-						break
-
+						exit(1)
 				case 'delete':
-						if (!param1) {
-								log_cli_error('DELETE', 'Task id was not provided')
-								exit(1)
-						}
-
-						if (Number.isNaN(+param1)) {
-								log_cli_error('DELETE', 'Task id must be a number')
-								exit(1)
-						}
-
-						if (!check_if_task_exists(+param1)) {
-								log_cli_error('DELETE', `Task with id ${param1} does not exist`)
-								exit(1)
-						}
-
+						run_id_expection('UPDATE', param1)
 						delete_task(+param1)
-						break
+						exit(1)
 
 				case 'list':
 						if (param1) {
-								if (Object.values(TASK_STATUSES).includes(param1 as TASK_STATUSES)) {
-										list_tasks(param1 as TASK_STATUSES)
+								if (Object.values(TaskStatus).includes(param1 as TaskStatus)) {
+										list_tasks(param1 as TaskStatus)
 								} else {
-										log_cli_error('LIST', `Task status format is incorrect, possible values are: ${Object.values(TASK_STATUSES).join(', ')}`)
+										log_cli_error('LIST', `Task status format is incorrect, possible values are: ${Object.values(TaskStatus).join(', ')}`)
 								}
 						} else {
 								list_tasks()
 						}
 
-				break
+						exit(1)
+
+				case 'mark-todo':
+				case 'mark-in-progress':
+				case 'mark-done':
+						run_id_expection(arg.toUpperCase(), param1)
+
+						let status: TaskStatus | null = null;
+
+						if (arg === 'mark-todo') {
+								status = TaskStatus.TODO
+						}
+
+						if (arg === 'mark-in-progress') {
+								status = TaskStatus.IN_PROGRESS
+						}
+
+						if (arg === 'mark-done') {
+								status = TaskStatus.DONE
+						}
+
+						if (!status) {
+								exit(1)
+						}
+
+						change_status(+param1, status)
+						exit(1)
+
+				case 'help':
+						log_help()
+						exit(1)
 
 				default:
+						console.error(`Unknown argument: ${arg}\n`)
+						log_help()
 						exit(1)
 		}
 }
